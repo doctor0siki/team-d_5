0a597fae6c6518b04bacc41c0cf7953e97dfd862
伊東直輝 <naoki.nwk0220@gmail.com>
2019-09-19 09:56:37 +0900

    eventテーブル追加

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Model\Dao\Event;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -7,6 +8,12 @@ use Slim\Http\Response;
 $app->get('/', function (Request $request, Response $response) {
 
     $data = [];
+
+    //アイテムDAOをインスタンス化します。
+    $event = new Event($this->db);
+
+    //アイテム一覧を取得し、戻り値をresultに格納します
+    $data["events"] = $event->getEventList();
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
