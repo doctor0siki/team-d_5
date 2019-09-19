f28b99981fa006ad5e1677adb6558d034f0022f1
伊東直輝 <naoki.nwk0220@gmail.com>
2019-09-19 19:18:09 +0900

    マイページの予約履歴機能の追加

--- app/Controller/mypage/mypage.php
+++ app/Controller/mypage/mypage.php
@@ -1,6 +1,8 @@
 <?php
 
+use Model\Dao\Trade;
 use Model\Dao\Event;
+use Model\Dao\Restaurant;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -8,12 +10,28 @@ use Slim\Http\Response;
 $app->get('/mypage', function (Request $request, Response $response) {
 
     $data = [];
+    $data["reserves"] = [];
 
     //アイテムDAOをインスタンス化します。
     $event = new Event($this->db);
+    $trade = new Trade($this->db);
+    $restaurant = new Restaurant($this->db);
+
+    $user_id = $this->session["user_info"]["id"];
+
+    // 予約状況を検索
+    $trade_datas = $trade->select(array("user_id" => $user_id), "", "", 10, true);
+    $num = 0;
+    foreach($trade_datas as $trade_data){
+        $num++;
+        $data["reserves"][$num] = $restaurant->select(array("id" => $trade_data["restaurant_id"]), "", "", 10, false);
+        
+        $data["reserves"][$num]["people_num"] = $trade_data["people_num"];
+    }
 
     //アイテム一覧を取得し、戻り値をresultに格納します
     $data["events"] = $event->getEventList();
+    // dd($data);
 
     // Render index view
     return $this->view->render($response, 'mypage/index.twig', $data);
